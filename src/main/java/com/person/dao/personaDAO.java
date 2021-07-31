6bfa7a093c549503173e3bcd6a4952ed61bb36aa
yanditv <34551585+yanditv@users.noreply.github.com>
2021-07-30 23:57:23 -0500

    Update

--- src/main/java/com/person/dao/personaDAO.java
+++
@@ -1,7 +0,0 @@
-package com.person.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.person.model.persona;
-public interface  personaDAO extends JpaRepository<persona, Long>{
-
-}
